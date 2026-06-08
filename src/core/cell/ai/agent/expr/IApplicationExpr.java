973b4314e810e7674dad86f5cf0b08df579e965a
fanggz <741610393@qq.com>
2026-06-08 21:50:28 +0800

    feat: add IP whitelist application config bridge

--- src/core/cell/ai/agent/expr/IApplicationExpr.java
+++ src/core/cell/ai/agent/expr/IApplicationExpr.java
@@ -17,6 +17,7 @@ import gpf.adur.data.Form;
 import gpf.adur.data.FormField;
 import gpf.adur.data.FormModel;
 import gpf.adur.user.User;
+import octo.cm.dto.app.IpWhitelistConfigDto;
 import octo.cm.util.ApplicationUtil;
 import octo.cm.util.EasyOperation;
 import octo.cm.util.FormToJsonConversionUtil;
@@ -74,8 +75,10 @@ public interface IApplicationExpr extends CellIntf {
             }
 
             appForm.setAttrValue(FormToJsonConversionUtil.PREFIX_NEED_UUID, true);
-            return FormToJsonConversionUtil
+            JSONObject appConfig = FormToJsonConversionUtil
                     .convert(appForm);
+            ApplicationUtil.exposeIpWhitelistConfig(appConfig, appForm);
+            return appConfig;
         }
     }
 
@@ -103,6 +106,8 @@ public interface IApplicationExpr extends CellIntf {
 
 
         try (IDao dao = IDaoService.newIDao()) {
+            IpWhitelistConfigDto ipWhitelistConfig = ApplicationUtil.takeIpWhitelistConfig(jsonObject);
+
             // 通过业务域去搜索
             String applicationCode = ApplicationUtil
                     .getDefaultPublishApplicationCode(observer);
@@ -114,6 +119,10 @@ public interface IApplicationExpr extends CellIntf {
             Form form = JsonToFormConversionUtil.convert(appForm, jsonObject);
 
             if (form == null) throw new RuntimeException("应用配置数据有误，无法进行转换");
+
+            if (ipWhitelistConfig != null) {
+                ApplicationUtil.setIpWhitelistConfig(form, ipWhitelistConfig);
+            }
 
             IFormMgr.get().updateForm(null, dao, form, observer);
             IApplicationDeploy.get().deploy(Progress.newOutput(), dao, form, observer);
